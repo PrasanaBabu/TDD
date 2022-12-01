913e8915f24632e1ae244c453f1531c5d826e7f3
PrasanaBabu <88841365+PrasanaBabu@users.noreply.github.com>
2022-12-01 11:20:40 +0530

    Refactored Driver

--- TDD/src/main/java/String/Driver.java
+++ TDD/src/main/java/String/Driver.java
@@ -4,49 +4,52 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-         int temp;
-         String swapTwoChars;
-         String polindrome;
-         String duplicateStr;
+         int choice;
+         String inputString;
          StringMethodsImpl s=new StringMethodsImpl();
-        do{
-            Scanner in=new Scanner(System.in);
-            System.out.println("Choose options: \n 1.swap last two characters \n  2.check polindrome or not\n 3. remove duplicates from string\n enter 0 to exit\n");
-            temp=in.nextInt();
-            in.nextLine();
-            switch(temp)
-            {
-                case 1:
-                    System.out.println("Enter string");
-                    swapTwoChars=in.next();
-                    System.out.println("Before swapping string is:\n"+swapTwoChars);
-                    System.out.println("After swapping string is:\n"+s.swapOnlyLastTwoCharacters(swapTwoChars));
-                    break;
-                case 2:
-                    System.out.println("Enter string\n");
-                    polindrome=in.nextLine();
-                    int k=s.palindromeOrNot(polindrome);
-                    if( k== -1)
-                    {
-                        System.out.println("You entered an empty string\n");
-                    }
-                    else if(s.palindromeOrNot(polindrome)==10) {
-                        System.out.println("Given string is Polindrome:\n");
-                    }
-                    else
-                    {
-                        System.out.println("Given string is not a Polindrome:\n");
-                    }
-                    break;
-                case 3:
-                    System.out.println("Enter string");
-                    duplicateStr=in.next();
-                    System.out.println("Before removing duplicates from the string is:\n"+duplicateStr);
-                    System.out.println("After Removing duplicates from the string is:\n"+s.removeDuplicatesFromString(duplicateStr));
-                    break;
+         do{
+             Scanner in=new Scanner(System.in);
+             System.out.println("" +
+                     " Choose options: \n" +
+                     " 1.Swap last two characters \n" +
+                     " 2.Check palindrome or not\n" +
+                     " 3.Remove duplicates from string\n" +
+                     "  Enter 0 to exit\n");
 
-
-            }
-        }while (temp!=0);
+             choice=in.nextInt();
+             in.nextLine();
+             switch(choice)
+             {
+                 case 1:
+                     System.out.println("Enter string");
+                     inputString=in.next();
+                     System.out.println("Before swapping string is:\n" + inputString);
+                     System.out.println("After swapping string is:\n"
+                             + s.swapOnlyLastTwoCharacters(inputString));
+                     break;
+                 case 2:
+                     System.out.println("Enter string\n");
+                     inputString=in.nextLine();
+                     if( s.palindromeOrNot(inputString) == -1)
+                     {
+                         System.out.println("You entered an empty string\n");
+                     }
+                     else if(s.palindromeOrNot(inputString) == 10) {
+                         System.out.println("Given string is Palindrome:\n");
+                     }
+                     else
+                     {
+                         System.out.println("Given string is not a Palindrome:\n");
+                     }
+                     break;
+                 case 3:
+                     System.out.println("Enter string");
+                     inputString=in.next();
+                     System.out.println("Before removing duplicates from the string is:\n" + inputString);
+                     System.out.println("After Removing duplicates from the string is:\n"
+                             + s.removeDuplicatesFromString(inputString) );
+                     break;
+             }
+         }while (choice!=0);
     }
 }
